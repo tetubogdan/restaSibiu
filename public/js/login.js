2442bee7071eeb315cb937d265182a23d3dcba88
tetubogdanflorin <tetubogdanflorin@gmail.com>
2024-04-06 11:40:28 +0300

    adaugare interfata de admin

--- public/js/login.js
+++ public/js/login.js
@@ -4,15 +4,13 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Login reușit
-        alert('Autentificare reușită!');
-        // Redirecționează utilizatorul după login, de exemplu, către pagina principală
-        window.location.href = 'index.html'; // Schimbă 'homepage.html' cu pagina dorită
-    })
-    .catch((error) => {
-        console.error('Eroare de autentificare:', error);
-        alert('Eroare la login: ' + error.message);
-    });
+// Dupa login reusit
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then((userCredential) => {
+    // Redirectionare catre admin.html dupa login reusit
+    window.location.href = 'admin.html';
+})
+.catch((error) => {
+    console.error('Eroare de autentificare:', error);
+    alert('Eroare la login: ' + error.message);
 });
