c2b4b9b09036a92382dff1e3b1a1beff2a567399
tetubogdanflorin <tetubogdanflorin@gmail.com>
2024-04-06 11:42:47 +0300

    am reparat login.js sa faca login in admin.html direct

--- public/js/login.js
+++ public/js/login.js
@@ -4,13 +4,17 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-// Dupa login reusit
-firebase.auth().signInWithEmailAndPassword(email, password)
-.then((userCredential) => {
-    // Redirectionare catre admin.html dupa login reusit
-    window.location.href = 'admin.html';
-})
-.catch((error) => {
-    console.error('Eroare de autentificare:', error);
-    alert('Eroare la login: ' + error.message);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Login reușit
+        alert('Autentificare reușită!');
+        // Redirecționează utilizatorul după login, de exemplu, către pagina principală
+        window.location.href = 'admin.html'; // Schimbă 'homepage.html' cu pagina dorită
+    })
+
+    
+    .catch((error) => {
+        console.error('Eroare de autentificare:', error);
+        alert('Eroare la login: ' + error.message);
+    });
 });
